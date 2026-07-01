aa8b8fb0eed8c7bbe1c13902f09598e9da031aec
Fakhrul-17 <muhamadfakhrularifin17@gmail.com>
2026-07-01 17:10:19 +0700

    Fix: update base path untuk repo baru

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Mission-Intermediate-Front-End-2B/',
+  base: '/Mission-Advanced-Front-End-1/'
 })
